56126a732cf1d22f049d32167ee07d7b3a7fef42
LeeYooBin <santosroberdan25@gmail.com>
2022-01-19 19:57:36 -0300

    04 - funções

--- 04 - Funções/main.js
+++ 04 - Funções/main.js
@@ -3,13 +3,13 @@ const imprimeMensagem = () => console.log('mensagem');
 imprimeMensagem();
 //2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
 const exibeNome = nome => console.log(nome);
-exibeNome('Roberdan');
+exibeNome('nome');
 //3 - crie uma função que receba um nome, um número, e um estilo musical (parâmetros) e exiba no console
 const func1 = (nome, numero, estiloMusical) => console.log(nome, numero, estiloMusical);
-func1('Roberdan', 20, 'Rock');
+func1('nome', 20, 'Estilo musical');
 //4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
 const func2 = (filme, musica) => console.log(filme, musica);
-func2('lotr', 'Jeremy');
+func2('filme', 'musica');
 
 
 
