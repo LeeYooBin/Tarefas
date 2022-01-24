f7f742a1343c7b806a6a1892803583e74057c4b8
LeeYooBin <santosroberdan25@gmail.com>
2022-01-24 16:52:06 -0300

    05 - Estruturas de dados complexos (Array)

---
+++ 05 - Estrutura de dados complexos (Array)/main.js
@@ -0,0 +1,24 @@
+// Crie um array que receba 5 itens e exiba no console.
+const arr = ['João', 'Pedro', 'Maria', 'Paulo', 'Joana'];
+//console.log(arr);
+
+// Utilize um método para adicionar um nome ao inicio do array.
+arr.unshift('Carlos');
+//console.log(arr);
+
+// Utilize um método para remover o último nome do array.
+arr.pop();
+//console.log(arr);
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+arr.push('Iolanda', 'Matheus');
+//console.log(arr);
+
+// Utilize um método para remover o primeiro nome do array.
+arr.shift();
+//console.log(arr);
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+const numbers = [7, 5, 6, 3, 8, 9, 2, 1, 4];
+//console.log(numbers);
+
