314f5afb715158eb28dd41ef14a3aea200974511
LeeYooBin <santosroberdan25@gmail.com>
2022-02-15 02:05:53 -0300

    commit

---
+++ desafio-react-01-contador/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './App.css';
+
+export default class App extends React.Component{
+  state = { counter: 0 };
+
+  increment = () => {
+    if(this.state.counter < 10) this.setState(this.setState({counter: this.state.counter + 1}));
+    else this.setState({counter: 0});
+  }
+
+  decrement = () => {
+    if(this.state.counter > 0) this.setState(this.setState({counter: this.state.counter - 1}));
+    else this.setState({counter: 10});
+  }
+  
+  render(){
+    return (
+      <div id="main">
+        <h1>Contador</h1>
+        <button onClick={this.increment}> + </button>
+        <h2 on>{this.state.counter}</h2>
+        <button onClick={this.decrement}> - </button>
+      </div>
+    );
+  }
+}
